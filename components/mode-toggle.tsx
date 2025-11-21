82bf38cd2e2b67b084a42064151a12c7742613ab
rizkyfauziilmi <rizkyfauziilmi@gmail.com>
2025-11-21 19:09:03 +0700

    Add ModeToggle to navbar
    Place ModeToggle next to the NavigationMenu by wrapping them in a flex
    container and importing the component. Translate ModeToggle UI strings
    to

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -21,18 +21,18 @@ export function ModeToggle() {
         <Button variant="outline" size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-          <span className="sr-only">Toggle theme</span>
+          <span className="sr-only">Ubah Mode Tampilan</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
+          Terang
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
+          Gelap
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
+          Sistem
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
