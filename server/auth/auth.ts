7588095bd178c03229bc5e5828e3d8b5a0071aa8
rizkyfauziilmi <rizkyfauziilmi@gmail.com>
2025-11-26 14:39:18 +0700

    Add Google social sign-in functionality to login and signup forms

--- server/auth/auth.ts
+++ server/auth/auth.ts
@@ -10,6 +10,13 @@ export const auth = betterAuth({
         enabled: true,
         autoSignIn: true
     },
+    socialProviders: {
+        google: { 
+            prompt: "select_account",
+            clientId: process.env.GOOGLE_CLIENT_ID as string, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+        }, 
+    },
 });
 
 type Session = typeof auth.$Infer.Session
