01a82da04c49f81a753d836d11cd90ab44fb0fcd
rizkyfauziilmi <rizkyfauziilmi@gmail.com>
2025-11-27 11:22:10 +0700

    feat(auth): add admin plugin

--- server/auth/auth.ts
+++ server/auth/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { db } from '../db';
+import { admin } from 'better-auth/plugins';
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
@@ -17,6 +18,7 @@ export const auth = betterAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
   },
+  plugins: [admin()],
 });
 
 export type Session = typeof auth.$Infer.Session;
